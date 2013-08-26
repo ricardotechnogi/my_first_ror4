633f52b5009338067f62b0054517f52c7150e54e
Ricardo Hernandez <ricardo@technogi.com.mx>
2013-08-26 18:32:29 +0000

    Changing tests commit

--- Gemfile
+++ Gemfile
@@ -4,13 +4,19 @@ source 'https://rubygems.org'
 gem 'rails', '4.0.0'
 
 # Use sqlite3 as the database for Active Record
-group :development do
+group :development, :test do
   gem 'sqlite3', '1.3.7'
+  gem 'rspec-rails', '2.13.1'
 end
 
 group :production do
   gem 'pg', '0.15.1'
   gem 'rails_12factor', '0.0.2'
+end
+
+group :test do
+  gem 'selenium-webdriver', '2.0.0'
+  gem 'capybara', '2.1.0'
 end
 
 gem 'sass-rails', '4.0.0'
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-FirstApp::Application.config.secret_key_base = 'feb5650b24ed0beb693d2fb92c11580e25fd84103448d5f825cb27b3b7ac4be217c7f169015949c5a136d553bef58633534ceb22220630728451339e8c96bb5f'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+FirstApp::Application.config.secret_key_base = secure_token
